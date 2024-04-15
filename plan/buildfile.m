c38619696652ac88257b551aeaf873e5b2c79d4c
pharouim <pharouim@FR-pharouim>
2024-04-15 14:04:35 +0200

    Modifying the buildfile function. Adding the builtin task functions check & test.

--- plan/buildfile.m
+++ plan/buildfile.m
@@ -1,36 +1,17 @@
 function plan = buildfile
-% Create a plan from the task functions
+%Create a plan from the task functions
 plan = buildplan(localfunctions);
 
-% Make the "checkLocalBranchUpToDate" task the default task in the plan
-plan.DefaultTasks = "run";
+%Set builtin check and test task
+plan("check")           = matlab.buildtool.tasks.CodeIssuesTask;
+plan("test")            = matlab.buildtool.tasks.TestTask;
 
-% Make dependencies
-plan(plan.DefaultTasks).Dependencies = ["check" "test"];
+%Make the "check" task the default task in the plan
+plan.DefaultTasks = "check";
 
-% cd ..
-% addpath(pwd)
-% cd plan
-end
+%Make dependencies
+plan(plan.DefaultTasks).Dependencies = ["test"];
 
-function checkTask(c)
-a = c;
-% Identify code issues
-issues = codeIssues;
-noError = isempty(issues.Issues) || ~any(ismember(issues.Issues.Severity, "error"));
-assert(noError,formattedDisplayText(issues.Issues))
-end
-
-function testTask(~)
-% Run unit tests 
-results = run(TestExtractValidationFunction);
-assertSuccess(results);
 end
 
 
-function runTask(~)
-% system("git fetch origin");
-% [~, msg] = system("git status");
-% assert(contains(msg, "Your branch is up to date with 'origin/main"), "Local repo is not up to date.")
-% % Check that local branch is up to date
-end
